873554355ffd57d24ad1d65779f8cdc105005903
Kevin <kevinroig37@gmail.com>
2023-03-27 18:02:09 +0200

    started opinions in easy admin

--- assets/index.js
+++ assets/index.js
@@ -10,3 +10,26 @@ import './styles/index.css';
 
 // start the Stimulus application
 import './bootstrap';
+
+/*JS for carousel on home page */
+const slidesContainer = document.getElementById("slides-container");
+
+const slide = document.querySelector(".slide");
+
+const prevButton = document.getElementById("slide-arrow-prev");
+
+const nextButton = document.getElementById("slide-arrow-next");
+nextButton.addEventListener("click", (event) => {
+
+    const slideWidth = slide.clientWidth;
+  
+      slidesContainer.scrollLeft += slideWidth;
+  
+  });
+  prevButton.addEventListener("click", () => {
+
+    const slideWidth = slide.clientWidth;
+  
+    slidesContainer.scrollLeft -= slideWidth;
+  
+  });
---
+++ src/Entity/Opinion.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OpinionRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OpinionRepository::class)]
+class Opinion
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $clientName = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $note = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getClientName(): ?string
+    {
+        return $this->clientName;
+    }
+
+    public function setClientName(?string $clientName): self
+    {
+        $this->clientName = $clientName;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getNote(): ?int
+    {
+        return $this->note;
+    }
+
+    public function setNote(?int $note): self
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+}
