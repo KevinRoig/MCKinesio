c6c0c8974a8a302399d8422b3a6c54774cb70bee
Kevin <kevinroig37@gmail.com>
2023-04-05 17:38:27 +0200

    contact form and controller created, working OK. Style to be done

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,17 +2,41 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\ContactType;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'contact')]
-    public function index(): Response
+    public function index(Request $request, MailerInterface $mailer): Response
     {
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $contactFormData = $form->getData();
+
+            $message = (new Email())
+                ->from($contactFormData['email'])
+                ->to('kevinroig@hotmail.com')
+                ->subject('Vous avez reçu une demande d\'informations')
+                ->text('Envoyé par : ' . $contactFormData['nom'] .' ' . $contactFormData['prenom'] .PHP_EOL .
+                    'Adresse mail :' .  $contactFormData['email'] . PHP_EOL .
+                    'Téléphone : ' . $contactFormData['telephone'] . PHP_EOL . 
+                    'Message du client : ' . $contactFormData['message']);
+                    $mailer->send($message);
+                    $this->addFlash('success', 'Votre message a bien été envoyé.');
+
+                    return $this->redirectToRoute('contact');
+        }
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            'form' => $form->createView(),
         ]);
     }
 }
